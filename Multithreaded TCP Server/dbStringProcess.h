d0fd19c7a2f49bf5a98dba40abaeaaadc8c7b8c7
John Saja CW <a7v8fzz@mmm.com>
2018-05-21 11:40:18 -0500

    Reorganizing file structure

--- Multithreaded TCP Server/dbStringProcess.h
+++
@@ -1,9 +0,0 @@
-#ifndef DBSTRINGPROCESS_H
-#define DBSTRINGPROCESS_H
-
-char* getHaystack(char* needle, char* database, char* lineBuf);
-char* makeOutputString(char* needle, char* haystack, char* partialHaystack, char* finalString);
-char* dbProcess(char* needle);
-char* getNeedle(FILE* clientFile, char* buffer);
-
-#endif /* DBSTRINGPROCESS_H */
--- Twitter Machine Learning Project/Text Classification Code/README.md
+++
@@ -1,3 +0,0 @@
-If you plan to use the text classification code, you will need to obtain your own API key.
-Additionally, refer to the "Tools and Software" portion of the PDF to see what softwares and
-dependencies you will need.
